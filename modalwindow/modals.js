17a55eedd53b386b1619d79d32ad317e09b5a2a3
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-07-28 23:06:22 -0700

    Refactor modals code

--- modalwindow/modals.js
+++ modalwindow/modals.js
@@ -7,26 +7,26 @@ const hidden = document.querySelector(".hidden");
 const btnClose = document.querySelector(".close-modal")
 const overlay = document.querySelector(".overlay")
 
-console.log(btnModal);
+// openModals open popup modal
+const openModals = () => {
+   hidden.classList.remove("hidden");
+   overlay.classList.remove("hidden");
+}
+
+// closeModals open of close modals
+const closeModals = () => {
+   hidden.classList.add("hidden");
+   overlay.classList.add("hidden");
+}
 
 // Add addEventListener to all three Show modal buttons
 for (let i = 0; i < btnModal.length; i++) {
    console.log(btnModal[i].textContent);
-   btnModal[i].addEventListener("click", function() {
-      console.log("Button Clien", i) 
-      hidden.classList.remove("hidden");
-      overlay.classList.remove("hidden");
-   });
+   btnModal[i].addEventListener("click", openModals);
 }
 
 // Remove the modal and overlay by click on the modal X button
-btnClose.addEventListener("click", function() {
-   hidden.classList.add("hidden");
-   overlay.classList.add("hidden");
-});
+btnClose.addEventListener("click", closeModals);
 
 // Remove the overlay, and the modal by click anywhere on the overlay 
-overlay.addEventListener("click", function() {
-   hidden.classList.add("hidden");
-   overlay.classList.add("hidden");
-});
+overlay.addEventListener("click", closeModals);
