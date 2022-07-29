21a06e6d3c5f490360cdc527aa1fa429eaa7ee87
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-07-28 22:18:40 -0700

    checking in Modals component example code

--- modelwindow/modals.js
+++ modelwindow/modals.js
@@ -1,8 +1,32 @@
 'use strict';
 
-const btnModal = document.getElementsByClassName(".show-modal");
-const model = document.getElementsByName(".model");
-const hidden = document.getElementsByName(".hidden");
-const btnClose = document.getElementsByName(".close-modal")
-const overlay = document.getElementsByName(".overlay")
+// Pre-declare variables for DOM querySelector classes on index.html
+const btnModal = document.querySelectorAll(".show-modal");
+const model = document.querySelector(".model");
+const hidden = document.querySelector(".hidden");
+const btnClose = document.querySelector(".close-modal")
+const overlay = document.querySelector(".overlay")
 
+console.log(btnModal);
+
+// Add addEventListener to all three Show modal buttons
+for (let i = 0; i < btnModal.length; i++) {
+   console.log(btnModal[i].textContent);
+   btnModal[i].addEventListener("click", function() {
+      console.log("Button Clien", i) 
+      hidden.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+   });
+}
+
+// Remove the modal and overlay by click on the modal X button
+btnClose.addEventListener("click", function() {
+   hidden.classList.add("hidden");
+   overlay.classList.add("hidden");
+});
+
+// Remove the overlay, and the modal by click anywhere on the overlay 
+overlay.addEventListener("click", function() {
+   hidden.classList.add("hidden");
+   overlay.classList.add("hidden");
+});
