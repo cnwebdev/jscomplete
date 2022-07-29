aee6df756eca8e04c9ec7056ec6d3ca7b2db7575
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-07-28 19:51:51 -0700

    Game logic refactor round n

--- numguessgame/ngg.js
+++ numguessgame/ngg.js
@@ -2,14 +2,14 @@
 
 //Global game messages
 const winmsg = "🏆 You Won!";
-const lossmsg = "Game over, try again?";
+const lossmsg = "💥 Game over, try again?";
 const highmsg = " ☝️️ Too high!";
 const lowmsg = "👇 Too low!";
 const oorang = "Try number from 1 to 20";
 const uiScoreMsg = "Start guessing...";
+const gameResetMsg = "reset";
 
 // HTML classes variables - from index.html
-const uiAlert = document.querySelector(".gamealert");
 const buttonReset = document.querySelector(".again");
 const uiRanNum = document.querySelector(".number");
 const uiGuess = document.querySelector(".guess");
@@ -30,6 +30,16 @@ const gameScores = {
    score: 20,
    highScore: 0,
 };
+
+const uiState = {
+   uiRanNum: uiRanNum,
+   uiGuess: uiGuess,
+   uiMessage: uiMessage,
+   uiScore: uiScore,
+   uiHighScore: uiHighScore,
+   uiBody: uiBody,
+}
+
 console.log(gameScores);
 
 // Event listenners
@@ -37,115 +47,107 @@ uiGuess.addEventListener("keypress", function (event) {
    if (event.key === "Enter") {
       let guessVal = uiGuess.value;
       console.log(guessVal, typeof guessVal)
-      processData(guessVal)
+      updateGameData(guessVal);
    }
 });
 
 buttonCheck.addEventListener("click", function () {
    let guessVal = uiGuess.value;
-   processData(guessVal);
+   updateGameData(guessVal);
 });
 
 buttonReset.addEventListener("click", function () {
    gameReset();
 });
 
-// gameReset restart the game
+// Play again data
 function gameReset() {
    ranNum = Math.trunc(Math.random() * 20) + 1;
-   console.log(ranNum);
-   gameScores.score = gameScores.highScore;
-   uiScore.innerHTML = gameScores.highScore;
-   uiMessage.innerHTML = uiScoreMsg;
-   uiGuess.value = " ";
-   uiBody.style.backgroundColor = "#222222";
-   uiRanNum.innerHTML = "?";
-   uiRanNum.style.width = "15rem";
-}
-
-
-// Compares userNum to ranNum 
-function matchScores() {
-   if (gameScores.usrInput === gameScores.pcNum) {
-      return winmsg;
-   } else if (gameScores.usrInput > gameScores.pcNum) {
-      return highmsg;
-   } else if (gameScores.usrInput < gameScores.pcNum) {
-      return lowmsg;
-   } else {
-      return oorang;
-   }
-}
-
-// Score to Game data object
-function populateGameData(userInput) {
-   let userNum = userInput;
-   gameScores.usrInput = userNum;
    gameScores.pcNum = ranNum;
-   console.log(gameScores);
-}
-
-// updateGameScores updates the score for each userInput strial
-function updateGameScores(scoreMsg) {
-   let msg = scoreMsg;
-   console.log(msg)
-   switch (msg) {
-      case winmsg:
-         gameScores.score += 1
-         gameScores.highScore = gameScores.score;
-         break;
-      case highmsg:
-      case lowmsg:
-         gameScores.score -= 1;
-         break;
-      case lossmsg:
-         gameScores.score = 0;
-         gameScores.highScore = 0;
-         break;
-      default:
-   }
-   console.log("From updateGameScores", gameScores)
-}
-
-// Update Game UI 
-function updateUI(scoreMsg) {
-   let uiMsg = scoreMsg;
-   if (uiMsg === winmsg) {
-      uiBody.style.backgroundColor = "#60b347"; 
-      uiRanNum.style.width = "30rem";
-      uiRanNum.innerHTML = gameScores.pcNum;
-      uiHighScore.innerHTML = gameScores.score;
-      uiMessage.innerHTML = winmsg;
-   } else if (uiMsg === lossmsg) {
-      uiMessage.innerHTML = lossmsg;
-      uiScore.innerHTML = gameScores.score;
-      uiHighScore.innerHTML = gameScores.highScore;
+   gameScores.usrInput = 0;
+   if (gameScores.highScore > 0) {
+      gameScores.score = gameScores.highScore;
+      console.log(gameScores);
    } else {
-      uiMessage.innerHTML = uiMsg;
-      uiScore.innerHTML = gameScores.score;
-      uiHighScore.innerHTML = gameScores.highScore;
+      gameScores.score = 20;
+      gameScores.highScore = 0;
+      console.log(gameScores);
    }
+   uiState.uiBody.style.backgroundColor = "#222222";
+   uiState.uiRanNum.innerHTML = "?";
+   uiState.uiGuess.value = "";
+   uiState.uiScore.innerHTML = gameScores.score;
+   uiState.uiMessage.innerHTML = uiScoreMsg;
 }
 
-// Game controller
-function processData(guessVal) {
-   let scoreMsg;
-   let userNum = Number(guessVal);
-   console.log(userNum, ranNum);
-   if (userNum > 0 && userNum < 21) {
-      if (gameScores.score < 1) {
-         scoreMsg = lossmsg;
-         updateGameScores(scoreMsg);
-         updateUI(scoreMsg);
+// matching user input number with pc generated random number
+function matchScores() {
+   let message;
+   if (gameScores.score > 0) {
+      if (gameScores.usrInput > 0 && gameScores.usrInput <= 20) {
+         if (gameScores.usrInput === gameScores.pcNum) {
+            message = winmsg;
+         } else if (gameScores.usrInput > gameScores.pcNum) {
+            message = highmsg;
+         } else if (gameScores.usrInput < gameScores.pcNum) {
+            message = lowmsg;
+         }
       } else {
-         populateGameData(userNum);
-         scoreMsg = matchScores();
-         console.log("from processData", scoreMsg);
-         updateGameScores(scoreMsg);
-         updateUI(scoreMsg);
+         message = oorang;
       }
    } else {
-      scoreMsg = oorang;
-      updateUI(scoreMsg);
+      message = lossmsg;
+   }
+   console.log("From matchScores", message);
+   return message;
+}
+
+// Update UI States 
+function updateUIStates(message) {
+   let scoreMsg = message;
+   console.log(scoreMsg);
+   console.log("from updateUIStates", scoreMsg)
+   if (message === winmsg) {
+      uiState.uiMessage.innerHTML = winmsg;
+      uiState.uiBody.style.backgroundColor = "#60b347";
+      uiState.uiRanNum.innerHTML = gameScores.pcNum;
+      uiState.uiScore.innerHTML = gameScores.score;
+      uiState.uiHighScore.innerHTML = gameScores.highScore;
+      uiState.uiRanNum.style.width = "30rem";
+   } else if (message === lossmsg ) {
+      uiState.uiMessage.innerHTML = lossmsg;
+   } else {
+      uiState.uiScore.innerHTML = gameScores.score;
+      uiState.uiHighScore.innerHTML = gameScores.highScore;
+      uiState.uiMessage.innerHTML = scoreMsg;
    }
 }
+
+
+// Score to Game data object
+function updateGameData(guessVal) {
+   let userNum = Number(guessVal);
+   gameScores.usrInput = userNum;
+   gameScores.pcNum = ranNum;
+   console.log("From updateGameDate", userNum, ranNum)
+   let message = matchScores();
+   if (message === winmsg) {
+      gameScores.score += 1;
+      gameScores.highScore = gameScores.score;
+      updateUIStates(message);
+   } else if (message === highmsg) {
+      gameScores.score -= 1;
+      updateUIStates(message);
+   } else if (message === lowmsg) {
+      gameScores.score -= 1;
+      updateUIStates(message);
+   } else if (message == oorang) {
+      updateUIStates(message);
+   } else if (message == lossmsg) {
+      gameScores.score = 20;
+      gameScores.highScore = 0;
+      updateUIStates(message);
+   }
+   console.log("from updateGameDate", gameScores);
+}
+
