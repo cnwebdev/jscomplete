32d6d52452d0e0a39318c26f880546ae0dc0d282
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-07-31 14:24:29 -0700

    add complete calculator code to repo

--- jsfeatures/basiccalculator/assets/scripts/aap.js
+++ jsfeatures/basiccalculator/assets/scripts/aap.js
@@ -9,28 +9,18 @@ const data = {
    result: 0,
 }
 
+// Function resetData is declared but not used in current version
 const resetData = () => {
    data.input = 0;
    data.counter = 0;
    data.val1 = 0;
    data.val2 = 0;
-   data.operator = "";
+   data.operator = "operator";
    data.result = 0;
 }
 
-const getInput = () => {
-   inputCounter();
-   data.input = Number(userInput.value);
-   if (data.counter === 1) {
-      data.val1 = data.input;
-   } else if (data.counter === 2) {
-      data.val2 = data.input;
-      computeData();
-   }
-   userInput.value = "";
-   console.log(data.input, typeof data.input);
-}
-
+// Set counter to redirect data.input value to data.val1 or data.val2
+// This counter only set to 1 or 2 
 const inputCounter = () => {
    if (data.counter < 0 || data.counter >= 2) {
       data.counter = 1;
@@ -39,11 +29,31 @@ const inputCounter = () => {
    }
 }
 
+// getInput get the data from input field, convert to Number then store 
+// in data object account to data.counter value of 1 or 2
+const getInput = () => {
+   inputCounter();
+   data.input = Number(userInput.value);
+   if (data.counter === 1) {
+      data.val1 = data.input;
+      data.val2 = 0;
+      data.operator = "operator";
+   } else if (data.counter === 2) {
+      data.val2 = data.input;
+      computeData();
+   }
+   console.log(data.input, typeof data.input);
+}
+
+// setOperator set the math operator to data.operator object
 const setOperator = (sign) => {
    let operator = sign;
    data.operator = operator;
+   data.val2 = 0;
 }
 
+// computeData calculates the math expression and store result 
+// in data.result object
 const computeData = () => {
    if (data.counter === 2) {
       let operator;
@@ -68,30 +78,44 @@ const computeData = () => {
    }
 }
 
+// outputResult prints data object feilds to the screen
+function outputResult(result, first, operator, second) {
+   currentResultOutput.textContent = result;
+   firstValEl.innerHTML = data.val1;
+   operatorEl.innerHTML = data.operator;
+   secondValEl.innerHTML = data.val2;
+   userInput.value = "";
+ }
+ 
+// input field event 
 userInput.addEventListener("keydown", function(e) {
    const key = e;
    if (e.key === "Enter") {
       getInput();
       outputResult(data.result, data.input)
+      console.log(data);
    }
-   console.log(data);
 });
 
+// addBtn event 
 addBtn.addEventListener("click", function() {
    setOperator("+");
    outputResult(data.result, data.operator)
 });
 
+// subtractBtn event
 subtractBtn.addEventListener("click", function() {
    setOperator("-");
-   outputResult(data.result, data.operator)
+   outputResult(data.result, data.operator)  
 });
 
+// multiplyBtn event
 multiplyBtn.addEventListener("click", function() {
    setOperator("*");
-   outputResult(data.result, data.operator)
+   outputResult(data.result, data.operator)  
 });
 
+// divideBtn event
 divideBtn.addEventListener("click", function() {
    setOperator("/");
    outputResult(data.result, data.operator)
--- jsfeatures/basiccalculator/assets/scripts/vendor.js
+++ jsfeatures/basiccalculator/assets/scripts/vendor.js
@@ -5,13 +5,12 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+const firstValEl = document.querySelector(".first-val");
+const operatorEl = document.querySelector(".operator");
+const secondValEl = document.querySelector(".second-val");
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
-function outputResult(result, text) {
-  currentResultOutput.textContent = result;
-  currentCalculationOutput.textContent = text;
-}
 
 
